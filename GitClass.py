4a2857a3abb0f0bd6bde74b7ca7c97ba9908e0df
OranFrydman <oranfry@post.bgu.ac.il>
2023-02-19 17:28:59 +0200

    Show how GUI works

--- GitClass.py
+++ GitClass.py
@@ -1,12 +1,1 @@
-print("My Secret  is 1234")
-
-print("Nice Secrert!")
-def plus (a):
-    return a+1
-a=5
-b=7
-print(plus(a))
-
-print("okay the func works")
-
-print("Created new Branch")
+print("My Secret  is 1234,Oran")
