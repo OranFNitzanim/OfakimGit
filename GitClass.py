df0c7397300fd8c263347d954096a0f91b5666b6
OranFrydman <oranfry@post.bgu.ac.il>
2023-02-22 17:39:38 +0200

    Add function to get text

--- GitClass.py
+++ GitClass.py
@@ -4,3 +4,11 @@ print("My Secret  is 1234,Oran")
 def Multi(a,b):
     print("Hello Shani how are you?")
     return a*b+1
+
+
+def get_text():
+    text = input("Enter Your text")
+    return text
+
+
+    
